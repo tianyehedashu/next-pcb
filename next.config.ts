549c5e95942b3b1915edd4631410b2450545edce
tianyehedashu <592395647@qq.com>
2025-04-29 12:28:30 +0800

    chore: ignore type check and eslint errors during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! 仅在生产构建时忽略类型错误
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // !! 仅在生产构建时忽略 ESLint 错误
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
