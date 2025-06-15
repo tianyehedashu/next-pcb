a74d087b74e2f26e09a57e56ab6d66601de9e545
tianyehedashu <592395647@qq.com>
2025-06-15 17:27:06 +0800

    修复字体错误

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,11 @@ import { ChatwootProvider } from '@/app/components/ChatwootProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
